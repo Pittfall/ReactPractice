4689dfd5c3158050856c96aa0f5dd2dffb2a7252
arusu <arusu@sehcomp.ca>
2018-06-22 11:41:14 -0400

    Added communication between 2 components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,54 @@ import './App.css';
 import Person from './Person/Person'; 
 
 class App extends Component {
+  state = {
+    people: [
+      { name: "Andrew", age: 30 },
+      { name: "Dora", age: 29 },
+      { name: "Jessica", age: 20 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    let newPeople = this.state.people.slice();
+    newPeople[0].name = (newPeople[0].name === newName) ? "Liviu" : newName;
+    this.setState({people: newPeople});
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      people: [
+        { name: "Andrew", age: 30 },
+        { name: event.target.value, age: 29 },
+        { name: "Jessica", age: 20 }
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
-        <Person name="Andrew" age="30">My hobby is fishing</Person>
+        <button style={style} onClick={this.switchNameHandler.bind(null, "Liviu")}>Switch Name</button>
+        <Person 
+        name={this.state.people[0].name} 
+        age = {this.state.people[0].age}>My hobby is fishing</Person>
+        <Person 
+        name={this.state.people[1].name} 
+        age = {this.state.people[1].age} 
+        click={this.switchNameHandler.bind(this, "Christina")} 
+        changed={this.nameChangedHandler} />
+        <Person 
+        name={this.state.people[2].name} 
+        age = {this.state.people[2].age} />
       </div>
     );
   }
