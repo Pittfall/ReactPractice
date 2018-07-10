6f4d7cfc09fa344ac166eaf4ebc8275e7ba4a93c
arusu <arusu@sehcomp.ca>
2018-07-10 11:57:05 -0400

    Added a higher level component to act as a wrapping component so that a wrapping div is not needed

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import classes from './Cockpit.css';
+import Hoc from './Hoc';
 
 const Cockpit = (props) => {
     const localClasses = [];
-    let btnClass = props.showPeople ? classes.Red : '';
+    let btnClass = props.showPeople ? [classes.Button, classes.Red].join(' ') : classes.Button;
 
     if (props.people.length <= 2) {
       localClasses.push(classes.red);
@@ -13,11 +14,11 @@ const Cockpit = (props) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Hoc>
             <h1>{props.title}</h1>
             <p className={localClasses.join(' ')}>This is really working!</p>
             <button className={btnClass} onClick={props.clicked}>Toggle People</button>
-        </div>
+        </Hoc>
     );
 }
 
