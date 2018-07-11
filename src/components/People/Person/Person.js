ac700b12ec65f542de986791aec8e19c30db0b35
arusu <arusu@sehcomp.ca>
2018-07-11 13:00:09 -0400

    Added a component wrapper class and moved HOC to an HOC folder for wrapper concepts or higher order components

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './Person.css';
+import Wrapped from '../.././Hoc/Wrapper'
 
 const person = (props) => {
     return (
-        <div className={classes.Person}>
+        <div>
             <p onClick={props.click}>Hi, my name is {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -11,4 +12,4 @@ const person = (props) => {
     )
 };
 
-export default person;
+export default Wrapped(person, classes.Person);
